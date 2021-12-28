aaf91c9888ff5c172cc09edcca729d0ea87cd2cd
Tina Shumilova <shutiva@gmail.com>
2021-12-28 17:34:56 +0200

    add methods CreateArray, FillArray, PrintArray and method Test

--- Test_1_2021/Program.cs
+++ Test_1_2021/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArray()
+{
+    int lenght = Convert.ToInt32(Console.ReadLine());
+    return new int[lenght];
+}
+
+int[] FillArray(int[] array)
+{
+    int min = Convert.ToInt32(Console.ReadLine());
+    int max = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+string PrintArray(int[] array)
+{
+    string text = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        text = text + $"{array[i]} ";
+    }
+    return text;
+}
+
+void TestMethods()
+{
+    int[] firstArray = CreateArray();
+    FillArray(firstArray);
+    Console.WriteLine(PrintArray(firstArray));
+}
+
+TestMethods();
